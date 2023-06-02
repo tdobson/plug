f5c08ab3aadba6cd4c5ae33e834eddafe59653c1
Glitch (glitch-hello-react) <none>
2023-06-02 22:17:02 +0000

    ⚓️🍁 Checkpoint ./src/app.jsx:53254092/261

--- src/app.jsx
+++ src/app.jsx
@@ -61,14 +61,7 @@ return (
 <span className="divider">|</span>
 <Link href="/about">About</Link>
 </div>
-<a
-       className="btn--remix"
-       target="_top"
-       href="https://glitch.com/edit/#!/remix/glitch-hello-react"
-     >
-<img src="https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FLogo_Color.svg?v=1618199565140" alt="" />
-Remix on Glitch
-</a>
+
 </footer>
 </Router>
 );
