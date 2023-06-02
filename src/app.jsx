31604272632035ae6c756248f6b4872de963bb59
Glitch (glitch-hello-react) <none>
2023-06-02 21:54:02 +0000

    🙈🎷 Checkpoint ./src/app.jsx:53254092/6097 ./package.json:53254092/70

--- src/app.jsx
+++ src/app.jsx
@@ -1,111 +1,75 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useMemo, useCallback } from 'react';
 import { Router, Link } from "wouter";
-
-/**
-* This code defines the react app
-*
-* Imports the router functionality to provide page navigation
-* Defines the Home function outlining the content on each page
-* Content specific to each page (Home and About) is defined in their components in /pages
-* Each page content is presented inside the overall structure defined here
-* The router attaches the page components to their paths
-*/
-
-// Import and apply CSS stylesheet
-import "./styles/styles.css";
-
-// Where all of our pages come from
-import PageRouter from "./components/router.jsx";
-
-// The component that adds our Meta tags to the page
-import Seo from './components/seo.jsx';
-
-// Home function that is reflected across the site
-export default function Home() {
-  return (
-    <Router>
-      <Seo />
-      <main role="main" className="wrapper">
-        <div className="content">
-          {/* Router specifies which component to insert here as the main content */}
-          <PageRouter />
-        </div>
-      </main>
-      {/* Footer links to Home and About, Link elements matched in router.jsx */}
-      <footer className="footer">
-        <div className="links">
-          <Link href="/">Home</Link>
-          <span className="divider">|</span>
-          <Link href="/about">About</Link>
-        </div>
-        <a
-          className="btn--remix"
-          target="_top"
-          href="https://glitch.com/edit/#!/remix/glitch-hello-react"
-        >
-          <img src="https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FLogo_Color.svg?v=1618199565140" alt="" />
-          Remix on Glitch
-        </a>
-      </footer>
-    </Router>
-  );
-}
-
-
-import React, { useState, useRef, useEffect, useMemo, useCallback} from 'react';
-import { createRoot } from 'react-dom/client';
 import { AgGridReact } from 'ag-grid-react'; // the AG Grid React Component
-
 import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
 import 'ag-grid-community/styles/ag-theme-alpine.css'; // Optional theme CSS
+import PageRouter from "./components/router.jsx";
+import Seo from './components/seo.jsx';
+import "./styles/styles.css";
 
-const App = () => {
+export default function Home() {
+const gridRef = useRef(); // Optional - for accessing Grid's API
+const [rowData, setRowData] = useState(); // Set rowData to Array of Objects, one Object per Row
+const [columnDefs, setColumnDefs] = useState([
+{ field: 'make', filter: true },
+{ field: 'model', filter: true },
+{ field: 'price' }
+]);
+const defaultColDef = useMemo(() => ({
+sortable: true
+}));
 
- const gridRef = useRef(); // Optional - for accessing Grid's API
- const [rowData, setRowData] = useState(); // Set rowData to Array of Objects, one Object per Row
+const cellClickedListener = useCallback(event => {
+console.log('cellClicked', event);
+}, []);
 
- // Each Column Definition results in one Column.
- const [columnDefs, setColumnDefs] = useState([
-   {field: 'make', filter: true},
-   {field: 'model', filter: true},
-   {field: 'price'}
- ]);
+useEffect(() => {
+fetch('https://www.ag-grid.com/example-assets/row-data.json')
+.then(result => result.json())
+.then(rowData => setRowData(rowData))
+}, []);
 
- // DefaultColDef sets props common to all Columns
- const defaultColDef = useMemo( ()=> ({
-     sortable: true
-   }));
+const buttonListener = useCallback(e => {
+gridRef.current.api.deselectAll();
+}, []);
 
- // Example of consuming Grid Event
- const cellClickedListener = useCallback( event => {
-   console.log('cellClicked', event);
- }, []);
-
- // Example load data from server
- useEffect(() => {
-   fetch('https://www.ag-grid.com/example-assets/row-data.json')
-   .then(result => result.json())
-   .then(rowData => setRowData(rowData))
- }, []);
-
- // Example using Grid's API
- const buttonListener = useCallback( e => {
-   gridRef.current.api.deselectAll();
- }, []);
-
- return (
-   <div>
-
-     {/* Example using Grid's API */}
-     <button onClick={buttonListener}>Push Me</button>
-
-     {/* On div wrapping Grid a) specify theme CSS Class Class and b) sets Grid size */}
-     <div className="ag-theme-alpine" style={{width: 500, height: 500}}>
-
-       <AgGridReact ref={gridRef} // Ref for accessing Grid's API rowData={rowData} // Row Data for Rows columnDefs={columnDefs} // Column Defs for Columns defaultColDef={defaultColDef} // Default Column Properties animateRows={true} // Optional - set to 'true' to have rows animate when sorted
-           rowSelection='multiple' // Options - allows click selection of rows onCellClicked={cellClickedListener} // Optional - registering for Grid Event /></div>
-   </div>
- );
-};
-
-export default App;
+return (
+<Router>
+<Seo />
+<main role="main" className="wrapper">
+<div className="content">
+<div>
+<button onClick={buttonListener}>Push Me</button>
+<div className="ag-theme-alpine" style={{ width: 500, height: 500 }}>
+<AgGridReact
+             ref={gridRef}
+             rowData={rowData}
+             columnDefs={columnDefs}
+             defaultColDef={defaultColDef}
+             animateRows={true}
+             rowSelection='multiple'
+             onCellClicked={cellClickedListener}
+           />
+</div>
+</div>
+<PageRouter />
+</div>
+</main>
+<footer className="footer">
+<div className="links">
+<Link href="/">Home</Link>
+<span className="divider">|</span>
+<Link href="/about">About</Link>
+</div>
+<a
+       className="btn--remix"
+       target="_top"
+       href="https://glitch.com/edit/#!/remix/glitch-hello-react"
+     >
+<img src="https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FLogo_Color.svg?v=1618199565140" alt="" />
+Remix on Glitch
+</a>
+</footer>
+</Router>
+);
+}
