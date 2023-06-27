a965808e5573ce61c1b042010e962086c77c13b4
tdobson <hello@tdobson.net>
2023-06-27 18:21:19 +0100

    added more debugging

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app.jsx";
-import { HelmetProvider } from 'react-helmet-async';
+import {HelmetProvider} from 'react-helmet-async';
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
 
 /**
-* Root of react site 
-*
-* Imports Helmet provider for the page head
-* And App which defines the content and navigation
-*/
+ * Root of react site
+ *
+ * Imports Helmet provider for the page head
+ * And App which defines the content and navigation
+ */
 
 // Render the site https://reactjs.org/docs/react-dom.html#render
 ReactDOM.render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <HelmetProvider>
+            <DevSupport ComponentPreviews={ComponentPreviews}
+                        useInitialHook={useInitial}
+            >
+                <App/>
+            </DevSupport>
+        </HelmetProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
