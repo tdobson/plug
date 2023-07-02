f8e13f2b303cd5be7f4852eeb0f833119c2ca44a
tdobson <hello@tdobson.net>
2023-07-02 02:02:21 +0100

    added another API and

--- src/api.jsx
+++ src/api.jsx
@@ -35,6 +35,23 @@ export async function fetchDetailsForMissingUserOrderIDs(product_id, userOrderID
             'climbing-indoors-toproping-grades',
             'climbing-indoors-skills-passing-on',
             'admin-first-timer-indoor',
+            'admin-wednesday-requests-notes',
+            'milestones_3_badge',
+            'milestones_5_band',
+            'stats_volunteer_for_numerator_cached',
+            'committee_current',
+            'cc_member',
+            'competency_indoor_trip_director',
+            'competency_indoor_checkin',
+            'competency_indoor_pairing',
+            'competency_indoor_floorwalker',
+            'competency_indoor_skillsharer',
+            'competency_indoor_announcements',
+            'cc_compliance_last_date_of_climbing',
+            'admin-code-of-conduct-accepted',
+            'admin-participation-statement-one',
+            'admin-participation-statement-two'
+
         ],
         order_meta_keys: ['cc_attendance', 'cc_volunteer', 'cc_volunteer_attendance'],
     };
@@ -49,4 +66,19 @@ export async function fetchDetailsForMissingUserOrderIDs(product_id, userOrderID
     });
 
     return await postResponse.json();
+}
+
+export async function sendOrderMeta(userOrderMeta) {
+    const headers = {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + authToken,
+    };
+
+    const response = await fetch('https://www.climbingclan.com/wp-json/wp-api/v1/send-order-meta', {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(userOrderMeta),
+    });
+
+    return await response.json();
 }
